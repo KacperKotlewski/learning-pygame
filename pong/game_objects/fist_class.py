b34b2b3f16ab76f4504efacb100639dd5121bf41
KacperKotlewski <kotlewski.kacper@gmail.com>
2020-02-22 22:29:33 +0100

    fist class complete

---
+++ pong/game_objects/fist_class.py
@@ -0,0 +1,22 @@
+from pygame import sprite, mouse
+from ..resources_meneger import load_image
+class Fist(sprite.Sprite):
+    def __init__(self):
+        super().__init__(self)
+        self.image, self.rect = load_image('fist.png', -1)
+        self.punching = False
+
+    def update(self):
+        pos = mouse.get_pos()
+        self.rect.midtop = pos
+        if self.punching:
+            self.rect.move_ip(5, 10)
+
+    def punch(self, target):
+        if not self.punching:
+            self.punching = True
+            hitbox = self.rect.inflate(-5,-5)
+            return hitbox.colliderect(target.rect)
+
+    def unpunch(self):
+        self.punching = False
